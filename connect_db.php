c584adcb9a5ca9325233ae8fc1f032feec4c95de
Chococoin <german.lugo@bitnibs.com>
2016-02-08 11:08:01 +0100

    encaletando date

--- connect_db.php
+++ connect_db.php
@@ -1,8 +1,8 @@
 <?php
 
-	$link = mysqli_connect("176.32.230.45","cl58-bitnibs","n4fB!3z9d3qV49E9^m","cl58-bitnibs");
+	$link = mysqli_connect("1...5","-","ninja","-");
 
 	if($link){
-		mysqli_select_db("cl58-bitnibs",$link)
+		mysqli_select_db("-",$link)
 	}
 ?>
