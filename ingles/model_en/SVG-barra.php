8b80fef4f58e12bfaffe8a09a77f61fa601c620e
Chococoin <german.lugo@bitnibs.com>
2016-02-08 14:53:45 +0100

    important modification

--- ingles/model_en/SVG-barra.php
+++ ingles/model_en/SVG-barra.php
@@ -1,6 +1,6 @@
 <div id="barra">
-<a href="#carrusel" id="logo" class="enelindex"><img src="../SVimagenes/logo.png"></a>
-<a href="../index.php" id="logo" class="interno"><img src="../SVimagenes/logo.png"></a>
+<a href="#carrusel" id="logo" class="enelindex"><img src="../../SVimagenes/logo.png"></a>
+<a href="../index_en.php" id="logo" class="interno"><img src="../../SVimagenes/logo.png"></a>
 
 <div id="botonera">
 
@@ -10,7 +10,7 @@
 <a href="#footer" class="item enelindex">contacto</a> 
 
 
-<a class="item idioma idiomaboton"><img src="../SVimagenes/spain.png"></a> 
+<a class="item idioma idiomaboton"><img src="../../SVimagenes/usa.png"></a> 
 
 
 
@@ -18,9 +18,9 @@
 </div>
 
 <div id="idiomas">
-<a id="fr" class="idioma" href="frances/index_fr.php"><img src="../SVimagenes/france.png"></a>
-<a id="usa" class="idioma" href="ingles/index_en.php"><img src="../SVimagenes/usa.png"></a>
-<a id="it" class="idioma" href="italiano/index_it.php"><img src="../SVimagenes/italy.png"></a>
+<a id="fr" class="idioma" href="../../frances/index_fr.php"><img src="../../SVimagenes/france.png"></a>
+<a id="usa" class="idioma" href="../../modelo/modelo_de_negocios.php"><img src="../../SVimagenes/spain.png"></a> 
+<a id="it" class="idioma" href="../../italiano/index_it.php"><img src="../../SVimagenes/italy.png"></a>
 
 </div>
 
